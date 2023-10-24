b50def0d3b672cc10204aac3337e77dbf0c33912
Felipe Ciro Montoya <fmontoya@soyhenry.com>
2023-10-23 23:14:42 -0500

    hw done

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -1,40 +1,86 @@
-'use strict'
+"use strict";
 // No cambies los nombres de las funciones.
+//
 
 function factorear(num) {
   // Factorear el número recibido como parámetro y devolver en un array
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
-  // Tu código:
-
+  // Tu código: [2,2,2,2]
+  // num = 5
+  // [1,3,5]
+  // factor = 5
+  // 5 % 5 -> 1
+  let factores = [1];
+  let factor = 2;
+  while (num > 1) {
+    if (num % factor == 0) {
+      factores.push(factor);
+      num = num / factor;
+    } else {
+      factor++;
+    }
+  }
+  return factores;
 }
-
+console.log(factorear(15));
 function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
 }
-
 
 function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  // i = 2
+  // j = 1
+  // aux = 2
+  //
+  // [2,3,5,6,1]
+  //        i
+  //j
+  for (let i = 1; i < array.length; i++) {
+    let j = i - 1; // j es la posicion
+    // arr[i] vs arr[j]
+    let aux = array[i];
+    while (j >= 0 && aux < array[j]) {
+      // i
+      array[j + 1] = array[j];
+      j--;
+    }
+    // j ??
+    array[j + 1] = aux;
+  }
+  return array;
 }
-
+console.log(insertionSort([5, 4, 3, 2, 1]));
 
 function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  // [1, 2, 5, 6, 3];
+  //     i
+  //              ^
+  // min = 4
+  for (let i = 0; i < array.length - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    let aux = array[i]; // 3
+    array[i] = array[min];
+    array[min] = aux;
+  }
+  return array;
 }
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------
--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 // No cambies los nombres de las funciones.
 
 function quickSort(array) {
@@ -6,15 +6,51 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  // array -> [5,6]
+  //              ^
+  // pivot -> array[0] -> 5
+  // recorrel el array y a medida que lo recorro, comparo con el pivote
+  // voy armando, array menores, mayores, iguales
+  // Condicion de corte
+  if (array.length < 2) return array;
+  // let indiceRandom = Math.floor(Math.random() * array.length);
+  let pivote = array[0]; // 5
+  let iguales = []; // [5] ->
+  let menores = []; // [] ->
+  let mayores = []; // [6] ->
 
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== pivote) {
+      if (array[i] < pivote) {
+        menores.push(array[i]);
+      } else {
+        mayores.push(array[i]);
+      }
+    } else {
+      iguales.push(array[i]);
+    }
+  }
+
+  // es necesario retornar la recursion?
+  // cual es la condicion de corte? -> Es la primera que se declara
+  // [2,1]  [3,3]  [5,6]
+  // [1,2,3,3]
+  //            [5,6]
+  // [1,2,3,3,5,6]
+  let arr = [1, 2, 3];
+  let arr2 = [...arr, ...[4]];
+  console.log(arr2);
+
+  return quickSort(menores).concat(iguales).concat(quickSort(mayores));
+  // return [...quickSort(menores), ...iguales, ...quickSort(mayores)]
 }
+console.log(quickSort([3, 2, 5, 1, 3, 6]));
 
 function mergeSort(array) {
   // Implementar el método conocido como mergeSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
   //okey probemos ahora
 }
 
